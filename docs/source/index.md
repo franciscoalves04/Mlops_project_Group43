b93d19e2e966389a49fe80503df2198f4448337a
Francisco Alves <francisco.pepetuoalves@gmail.com>
2026-01-09 21:36:42 +0100

    Initial commit - projeto Eye Deseases Classification

---
+++ docs/source/index.md
@@ -0,0 +1,3 @@
+## Documentation
+
+Documentation for eye_deseases_classification
---
+++ src/eye_deseases_classification/train.py
@@ -0,0 +1,10 @@
+from eye_deseases_classification.model import Model
+from eye_deseases_classification.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
