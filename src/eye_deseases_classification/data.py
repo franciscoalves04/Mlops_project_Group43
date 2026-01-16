Normalization error fix

src/eye_deseases_classification/data.py
@@ -4,6 +4,8 @@ from torch.utils.data import Dataset
 from PIL import Image
 from tqdm import tqdm
 import torch
+import numpy as np
+
 
 EXTENSIONS = {".jpg", ".jpeg", ".png"}
 IMAGE_SIZE = (256, 256)
@@ -33,13 +35,19 @@ class MyDataset(Dataset):
 
     def __getitem__(self, index):
         img_path, label = self.samples[index]
+
         img = Image.open(img_path).convert("RGB")
         img = img.resize(IMAGE_SIZE, Image.BILINEAR)
-        # Normalize to [0,1]
-        img = torch.tensor(torch.ByteTensor(torch.ByteStorage.from_buffer(img.tobytes())).float()).view(3, IMAGE_SIZE[0], IMAGE_SIZE[1]) / 255.0
+
+        # PIL -> numpy -> torch tensor
+        img = np.array(img, dtype=np.float32) / 255.0   # (H, W, C)
+        img = torch.from_numpy(img).permute(2, 0, 1)    # (C, H, W)
+
         if self.transform:
             img = self.transform(img)
+
         return img, label
+
 
     def preprocess(self, output_folder: Path):
         """Preprocess raw data into train/val/test splits."""

src/eye_deseases_classification/train.py
@@ -5,11 +5,12 @@ from eye_deseases_classification.model import ResNet
 from eye_deseases_classification.data import MyDataset
 from pathlib import Path
 import csv
+import numpy as np
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
 BATCH_SIZE = 16
-EPOCHS = 5
+EPOCHS = 50
 LEARNING_RATE = 1e-3
 
 # Paths
@@ -32,7 +33,7 @@ def train():
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-    # CSV file to log metrics
+    # CSV file - metrics
     csv_file = DOCS_DIR / "training_metrics.csv"
     with open(csv_file, mode="w", newline="") as f:
         writer = csv.writer(f)